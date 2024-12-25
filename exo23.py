77d2dc2fcbccd341fe6240f9a3a056864dcc2ef2
Ahleeem <kiheliahlem2@gmail.com>
2024-12-25 13:15:11 +0100

    exo 22/23

---
+++ exo23.py
@@ -0,0 +1,15 @@
+
+while True:
+    try:
+        N = int(input("Please enter a positive integer: "))
+        if N <= 0:
+            print("The number must be positive. Try again.")
+            continue
+        break 
+    except ValueError:
+        print("Invalid input. Please enter a valid integer.")
+
+
+for i in range(-N, N + 1):
+    if i != 0: 
+        print(i)
