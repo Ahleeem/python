db030dfa76df8596d22bade89327bd00af2be24c
Ahleeem <kiheliahlem2@gmail.com>
2024-12-25 13:19:15 +0100

    exo24

--- exo24.py
+++ exo24.py
@@ -6,9 +6,8 @@ def anagrams(str1, str2):
 
     return sorted(str1) == sorted(str2)
 
-# Test cases
-print(anagrams("tame", "meta"))  # True
-print(anagrams("tame", "mate"))  # True
-print(anagrams("tame", "team"))  # True
-print(anagrams("tabby", "batty"))  # False
-print(anagrams("python", "java"))  # False
+print(anagrams("tame", "meta")) 
+print(anagrams("tame", "mate"))  
+print(anagrams("tame", "team"))  
+print(anagrams("tabby", "batty"))  
+print(anagrams("python", "java")) 
